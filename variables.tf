7589e3c99d72710bad5e2c1934f10f1ce537f7ab
Dheimy <jheimyeng@hotmail.com>
2025-04-24 21:40:07 -0300

    ajust terraform

--- variables.tf
+++ variables.tf
@@ -1,132 +1,166 @@
-# Declaring missing variables
+# Example variables.tf file
 
 variable "cluster_name" {
-  description = "The name of the ECS cluster"
+  description = "Name of the ECS cluster"
   type        = string
 }
 
 variable "enable_container_insights" {
-  description = "Enable ECS container insights"
+  description = "Whether to enable container insights"
   type        = bool
 }
 
 variable "tags" {
-  description = "Tags to apply to the resources"
+  description = "Tags to apply to resources"
   type        = map(string)
 }
 
+variable "task_family" {
+  description = "ECS Task Definition family"
+  type        = string
+}
+
+variable "task_cpu" {
+  description = "ECS Task CPU"
+  type        = string
+}
+
+variable "task_memory" {
+  description = "ECS Task memory"
+  type        = string
+}
+
 variable "execution_role_arn" {
-  description = "The ARN of the ECS task execution role"
+  description = "ARN of the task execution role"
   type        = string
 }
 
 variable "task_role_arn" {
-  description = "The ARN of the ECS task role"
-  type        = string
-}
-
-variable "log_group_name" {
-  description = "The log group name for ECS task logs"
-  type        = string
-}
-
-variable "aws_region" {
-  description = "AWS region to deploy resources in"
+  description = "ARN of the task role"
   type        = string
 }
 
 variable "container_name" {
-  description = "The name of the container in the ECS task"
+  description = "Name of the container in the ECS task definition"
   type        = string
 }
 
 variable "container_image" {
-  description = "The container image to use"
+  description = "Docker image for the container"
   type        = string
 }
 
 variable "container_port" {
-  description = "The port the container listens on"
+  description = "Port on which the container listens"
   type        = number
 }
 
 variable "container_environment" {
-  description = "The environment variables for the container"
-  type = list(object({
-    name  = string
-    value = string
-  }))
+  description = "Environment variables for the container"
+  type        = list(map(string))
 }
 
 variable "container_secrets" {
   description = "Secrets for the container"
-  type = list(object({
-    name      = string
-    valueFrom = string
-  }))
+  type        = list(map(string))
 }
 
-variable "subnet_ids" {
-  description = "The list of subnet IDs for the ECS service"
-  type        = list(string)
-}
-
-variable "ecs_security_group_id" {
-  description = "The ECS security group ID"
+variable "log_group_name" {
+  description = "Log group name for ECS task logs"
   type        = string
 }
 
-variable "target_group_arn" {
-  description = "The ARN of the ECS target group"
+variable "aws_region" {
+  description = "AWS region"
   type        = string
 }
 
-variable "alb_listener_arn" {
-  description = "The ARN of the Application Load Balancer listener"
+variable "service_name" {
+  description = "Name of the ECS service"
   type        = string
 }
 
-variable "internal_alb" {
-  description = "Whether the ALB is internal or not"
-  type        = bool
-}
-
-variable "security_groups" {
-  description = "The security groups for the ALB"
-  type        = list(string)
-}
-
-variable "vpc_id" {
-  description = "The VPC ID where the ECS service and ALB will be deployed"
+variable "cluster_id" {
+  description = "ID of the ECS cluster"
   type        = string
 }
 
-variable "alb_port" {
-  description = "The port on which the ALB will listen"
-  type        = number
-}
-
-variable "alb_ingress_cidr_blocks" {
-  description = "The CIDR blocks allowed for ingress to the ALB"
-  type        = list(string)
-}
-
-variable "prod_target_group_name" {
-  description = "The target group name for production"
-  type        = string
-}
-
-variable "blue_target_group_name" {
-  description = "The target group name for blue deployment"
-  type        = string
-}
-
-variable "codedeploy_service_role_arn" {
-  description = "The ARN of the CodeDeploy service role"
+variable "task_definition_arn" {
+  description = "ARN of the ECS task definition"
   type        = string
 }
 
 variable "desired_count" {
-  description = "The desired number of ECS tasks"
+  description = "Desired number of running ECS tasks"
   type        = number
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for ECS service"
+  type        = list(string)
+}
+
+variable "ecs_security_group_id" {
+  description = "Security group ID for ECS tasks"
+  type        = string
+}
+
+variable "assign_public_ip" {
+  description = "Whether to assign public IP to ECS tasks"
+  type        = bool
+}
+
+variable "target_group_arn" {
+  description = "ARN of the target group for the ECS service"
+  type        = string
+}
+
+variable "alb_listener_arn" {
+  description = "ARN of the ALB listener"
+  type        = string
+}
+
+variable "alb_port" {
+  description = "Port for the ALB listener"
+  type        = number
+}
+
+variable "health_check_path" {
+  description = "Path for health checks"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "internal_alb" {
+  description = "Whether the ALB is internal"
+  type        = bool
+}
+
+variable "security_groups" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
+
+variable "alb_ingress_cidr_blocks" {
+  description = "CIDR blocks for ALB ingress rules"
+  type        = list(string)
+}
+
+variable "codedeploy_service_role_arn" {
+  description = "ARN of the CodeDeploy service role"
+  type        = string
+}
+
+variable "prod_target_group_name" {
+  description = "Name of the prod target group for ALB"
+  type        = string
+}
+
+variable "blue_target_group_name" {
+  description = "Name of the blue target group for ALB"
+  type        = string
 }
