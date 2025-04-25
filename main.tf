934e22a5cad165df94918dcde89727f990ca1593
Dheimy <jheimyeng@hotmail.com>
2025-04-25 01:32:23 -0300

    Ajust

--- main.tf
+++ main.tf
@@ -84,52 +84,6 @@ resource "aws_ecs_service" "main" {
 }
 
 
-# Security Groups
-resource "aws_security_group" "alb" {
-  name        = "${var.cluster_name}-alb-sg"
-  description = "Security group for ALB"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = var.alb_port
-    to_port     = var.alb_port
-    protocol    = "tcp"
-    cidr_blocks = var.alb_ingress_cidr_blocks
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = var.tags
-}
-
-resource "aws_security_group" "ecs_tasks" {
-  name        = "${var.cluster_name}-ecs-tasks-sg"
-  description = "Security group for ECS tasks"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port       = var.container_port
-    to_port         = var.container_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = var.tags
-}
-
-
 # CodeDeploy
 resource "aws_codedeploy_app" "main" {
   name             = "${var.cluster_name}-app"
