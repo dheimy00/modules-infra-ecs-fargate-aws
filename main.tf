2e45496a117c7b72224563cd3bbefeb8bebd7129
Dheimy <jheimyeng@hotmail.com>
2025-04-25 01:29:34 -0300

    Ajust

--- main.tf
+++ main.tf
@@ -84,48 +84,6 @@ resource "aws_ecs_service" "main" {
 }
 
 
-# ALB
-resource "aws_lb" "main" {
-  name               = "${var.cluster_name}-alb"
-  internal           = var.internal_alb
-  load_balancer_type = "application"
-  security_groups    = var.security_groups
-  subnets            = var.subnet_ids
-
-  tags = var.tags
-}
-
-resource "aws_lb_target_group" "main" {
-  name        = "${var.cluster_name}-tg"
-  port        = var.container_port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    timeout             = 5
-    interval            = 30
-    path                = var.health_check_path
-    matcher             = "200"
-  }
-
-  tags = var.tags
-}
-
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = var.alb_port
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
-
-
 # Security Groups
 resource "aws_security_group" "alb" {
   name        = "${var.cluster_name}-alb-sg"
