a1d788819b12d09bc380da18d6467e6d6eaa9a6d
Dheimy <jheimyeng@hotmail.com>
2025-05-29 03:47:09 -0300

    Ajust modules

--- outputs.tf
+++ outputs.tf
@@ -56,4 +56,8 @@ output "log_group_name" {
 output "alb_security_group_id" {
   description = "The ID of the security group for the ALB"
   value       = aws_security_group.alb.id
-} 
+}
+
+output "ecs_tasks_security_group_id" {
+  value = aws_security_group.ecs_tasks.id
+}
